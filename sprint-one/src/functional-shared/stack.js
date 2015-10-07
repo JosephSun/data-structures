afedbc2de4c5eba3e5a1e9b469096624566de771
JosephSun <bazzkhurshid@yahoo.com>
2015-10-07 16:34:21 -0700

    completed first test for function-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.index = 0;
+  // _.extend(obj, stackMethods);
+  obj.size = stackMethods.size; 
+  return obj; 
 };
 
 var stackMethods = {};
+stackMethods.size =  function(){
+  return this.index; 
+} 
 
+
+
+var myStack = Stack();
+console.log("obj that is stack", myStack,"mystack.size", myStack.size, "mystack.size()", myStack.size())
+myStack.size();
 
