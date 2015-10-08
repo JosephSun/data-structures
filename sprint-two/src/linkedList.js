12be147c727a5bd2679253379c5a30122decfb69
JosephSun <bazzkhurshid@yahoo.com>
2015-10-08 16:00:18 -0700

    completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -14,13 +14,28 @@ var LinkedList = function(){
   };
 
   list.removeHead = function(){
+    var removedHeadValue = list.head.value;
     var afterCurrentHead = list.head.next;
-    console.log("List.head.next", afterCurrentHead);
+    // console.log("List.head.next", afterCurrentHead);
     delete list.head;
     list.head = afterCurrentHead;
+    // console.log("List", list, "removedHead", removedHeadValue);
+    return removedHeadValue;
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+
+    while(true){
+      if(currentNode.value === target){
+        return true;
+      }
+      if(currentNode.next === null){
+        return false;
+      }
+      currentNode = currentNode.next;
+    }
+
   };
 
   return list;
