2b65e99434e1dd428743f46050727e07ce6330c7
JosephSun <bazzkhurshid@yahoo.com>
2015-10-09 16:43:04 -0700

    completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  var containsItem = false;//for readability we are adding this variable name
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      containsItem = true;
+      return containsItem; // putting this in here for faster algorithim time. 
+    }
+  }
+  return containsItem;
 };
 
 setPrototype.remove = function(item){
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      this.storage.splice(i,1);
+    }
+  }
+
 };
 
 /*
