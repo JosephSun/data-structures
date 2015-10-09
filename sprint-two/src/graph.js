f9ac4bc53cd0817a74cde78075284013a0320a25
JosephSun <bazzkhurshid@yahoo.com>
2015-10-08 21:27:18 -0700

    partially completed graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,12 +1,23 @@
 
 
 var Graph = function(){
+  this.storage = {};
+  this.index = 0;
 };
 
 Graph.prototype.addNode = function(node){
+  this.storage[this.index] = Node(node);
+  this.index++;
 };
 
 Graph.prototype.contains = function(node){
+  var foundNode = false;
+  for (var property in this.storage){
+    if(this.storage[property].value === node){
+      foundNode = true;
+    }
+  }
+  return foundNode;
 };
 
 Graph.prototype.removeNode = function(node){
@@ -28,5 +39,9 @@ Graph.prototype.forEachNode = function(cb){
  * Complexity: What is the time complexity of the above functions?
  */
 
-
-
+function NodeForGraph(val){
+  var node = {
+    value: val
+  };
+  return node;
+}
