cd4c758a6900db19558b5569430714056bbc8e64
JosephSun <bazzkhurshid@yahoo.com>
2015-10-10 11:37:48 -0700

    add time complexity comments

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,6 +3,9 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
+
+  //Time Complexity comment: O(1) === constant time because we are creating a new Node (which has a constant time operation), changing variables refrences and checking variable refrences. All these operations are constant time.  
+  //
   list.addToTail = function(value){
     var newNode = Node(value);
     if(list.head === null) {
@@ -13,6 +16,7 @@ var LinkedList = function(){
     list.tail = newNode;
   };
 
+  //Time Complexity comment: O(1) == constant time because we are only doing instant(by instant, we mean that there is no iteration, recursion, etc... all our operations happen 'instantly')  operations here.
   list.removeHead = function(){
     var removedHeadValue = list.head.value;
     var afterCurrentHead = list.head.next;
@@ -21,6 +25,8 @@ var LinkedList = function(){
     return removedHeadValue;
   };
 
+
+  //Time Complexity comment:O(n)  === linear time because we search the list here.  
   list.contains = function(target){
     var currentNode = list.head;
 
@@ -33,7 +39,6 @@ var LinkedList = function(){
       }
       currentNode = currentNode.next;
     }
-
   };
 
   return list;
@@ -50,4 +55,5 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ commented the answer before each function. The label of the comment is time complexity 
  */
