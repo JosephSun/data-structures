abcd082b2c81c6f7b3dca967728c1bd796f1c48a
JosephSun <bazzkhurshid@yahoo.com>
2015-10-09 19:11:05 -0700

    added comments to hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,14 +6,14 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
-   if (this._storage.get(i) !== null && this._storage.get(i) !== undefined){
-    // this._storage.set(i + 1,v); does not work to stop the collision
+    // console.log(this._storage.get(i));
+   // if (this._storage.get(i) !== null && this._storage.get(i) !== undefined){
+   //  this._storage.set(i + 1,v); //does not work to stop the collision
     //this.index++;
      // console.log('i', i, 'this._storage', this._storage, "this._storage.get(i)", this._storage.get(i));
-  }else {
+  //}else {
     this._storage.set(i,v);
-  }
+  //}
 
 };
 
@@ -26,7 +26,7 @@ HashTable.prototype.retrieve = function(k){
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k,this._limit);
-  console.log(this._storage.get(i));
+  //console.log(this._storage.get(i));
   if (this._storage.get(i) !== undefined) {
     this._storage.each(function(value, index, collection) {
       if (index === i) {
