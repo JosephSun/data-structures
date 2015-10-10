e2b04f3f3c36e82eedf47262edcf6ed9243d5b41
JosephSun <bazzkhurshid@yahoo.com>
2015-10-09 21:26:28 -0700

    complete binary search three

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,59 @@
 var BinarySearchTree = function(value){
+  this.left = null;
+  this.right = null;
+  this.value = value;
+
 };
 
+BinarySearchTree.prototype.insert = function(value){
+  var recurseTree = function(node){
+    if(node.value > value){
+      if(node.left === null){
+        node.left = new BinarySearchTree(value);
+      } else {
+        recurseTree(node.left)
+      }
+    } else {
+        if(node.right === null){
+          node.right = new BinarySearchTree(value);
+        } else {
+          recurseTree(node.right);
+        }
+      }   
+  }
+  recurseTree(this);
+};
+
+
+
+BinarySearchTree.prototype.contains = function(value){
+  var nodeInTree = false;
+  var recurseTree = function(node) {
+    if (node !== null){    
+      if (node.value === value) {
+        nodeInTree = true;
+      }else if (node.value > value) {
+        recurseTree(node.left);
+      }else {
+        recurseTree(node.right);
+      }
+    }
+
+  };
+  recurseTree(this);
+  return nodeInTree;
+};
+BinarySearchTree.prototype.depthFirstLog = function(func){
+
+  var recurseTree = function(node) {
+    if (node !== null) {
+      func(node.value);
+      recurseTree(node.left);
+      recurseTree(node.right);
+    }
+  };
+  recurseTree(this);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
